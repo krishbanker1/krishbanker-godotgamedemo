ae752c6b567ff36c467d16aae09d44c7a9b279b2
Krish <mr360degreebgmi@gmail.com>
2026-06-18 15:07:51 +0530

    Add on-screen turn controls

--- scripts/mobile_controls.gd
+++ scripts/mobile_controls.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 var player: Node
 var move_value := Vector2.ZERO
+var turn_value := 0.0
 
 func _ready() -> void:
 	player = get_node_or_null("../Player")
@@ -9,6 +10,8 @@ func _ready() -> void:
 	_add_button("Back", "S", Vector2(96, -72), Vector2(0, 1))
 	_add_button("Left", "A", Vector2(24, -72), Vector2(-1, 0))
 	_add_button("Right", "D", Vector2(168, -72), Vector2(1, 0))
+	_add_turn_button("TurnLeft", "<", Vector2(-280, -176), -1.0)
+	_add_turn_button("TurnRight", ">", Vector2(-144, -176), 1.0)
 	_add_action_button("Break", "Break", Vector2(-280, -96), "request_break")
 	_add_action_button("Place", "Place", Vector2(-144, -96), "request_place")
 
@@ -25,6 +28,23 @@ func _add_button(node_name: String, text: String, pos: Vector2, direction: Vecto
 	button.offset_bottom = pos.y + 64
 	button.button_down.connect(func(): _set_move(direction))
 	button.button_up.connect(func(): _set_move(Vector2.ZERO))
+	add_child(button)
+
+func _add_turn_button(node_name: String, text: String, pos: Vector2, direction: float) -> void:
+	var button := Button.new()
+	button.name = node_name
+	button.text = text
+	button.custom_minimum_size = Vector2(120, 64)
+	button.anchor_left = 1.0
+	button.anchor_top = 1.0
+	button.anchor_right = 1.0
+	button.anchor_bottom = 1.0
+	button.offset_left = pos.x
+	button.offset_top = pos.y
+	button.offset_right = pos.x + 120
+	button.offset_bottom = pos.y + 64
+	button.button_down.connect(func(): turn_value = direction)
+	button.button_up.connect(func(): turn_value = 0.0)
 	add_child(button)
 
 func _add_action_button(node_name: String, text: String, pos: Vector2, method_name: StringName) -> void:
@@ -44,7 +64,9 @@ func _add_action_button(node_name: String, text: String, pos: Vector2, method_na
 	add_child(button)
 
 func _process(_delta: float) -> void:
-	if Input.is_key_pressed(KEY_Q):
+	if turn_value != 0.0:
+		_call_player("add_mobile_look", Vector2(turn_value * 5.0, 0))
+	elif Input.is_key_pressed(KEY_Q):
 		_call_player("add_mobile_look", Vector2(-4, 0))
 	elif Input.is_key_pressed(KEY_E):
 		_call_player("add_mobile_look", Vector2(4, 0))
